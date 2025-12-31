c83d7594ff82e30933ab8bed9235ba43e0766a80
ricardoc-costa <ricardocccorreia@gmail.com>
2025-12-31 09:43:03 -0300

    Inicio da vinculação com a API

---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,25 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import { Container } from './styles'
+
+function Slider({ info, title }) {
+  return (
+    <Container>
+      <h2>${title}</h2>
+      <Swiper
+        grabCursor={true}
+        spaceBetween={10}
+        slidesPerView={'auto'}
+        className="swiper"
+      >
+        {info.map((item, index) => (
+          <SwiperSlide key={index}>
+            <div style={{ color: 'white' }}>{item.title}</div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Container>
+  )
+}
+
+export default Slider
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react'
 
 import Button from '../../components/Button/index.jsx'
+import Slider from '../../components/Slider'
 import api from '../../services/api'
+import { getImages } from '../../utils/getImages.js'
 import { Background, Info, Poster, Container, ContainerButtons } from './styles'
 
 function Home() {
   const [movie, setMovie] = useState()
+  const [topMovies, setTopMovies] = useState()
 
   useEffect(() => {
     async function getMovies() {
@@ -16,15 +19,23 @@ function Home() {
       setMovie(results[0])
     }
 
+    async function getTopMovies() {
+      const {
+        data: { results }
+      } = await api.get('/movie/top_rated')
+
+      console.log(results)
+      setTopMovies(results)
+    }
+
     getMovies()
+    getTopMovies()
   }, [])
 
   return (
     <>
       {movie && (
-        <Background
-          img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-        >
+        <Background img={getImages(movie.backdrop_path)}>
           <Container>
             <Info>
               <h1>{movie.title}</h1>
@@ -35,14 +46,12 @@ function Home() {
               </ContainerButtons>
             </Info>
             <Poster>
-              <img
-                alt="cpa-do-filme"
-                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-              />
+              <img alt="cpa-do-filme" src={getImages(movie.poster_path)} />
             </Poster>
           </Container>
         </Background>
       )}
+      {topMovies && <Slider info={topMovies} title={'Top Filmes'} />}
     </>
   )
 }
